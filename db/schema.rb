b960110f7b7df2a386161c827c16a9a6628a91a2
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-05-22 22:59:00 +0530

    chore: rollback previous migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_180529) do
-  create_table "recipes", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.text "ingredients"
-    t.text "instructions"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end

b8003b2f9533905ed7ee83e82f1c89f61811a53e
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-05-23 00:23:25 +0530

    chore: defined associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_173955) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "diets", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "instructions"
+    t.integer "prep_time"
+    t.integer "cook_time"
+    t.integer "total_time"
+    t.integer "servings"
+    t.text "ingredients"
+    t.integer "category_id", null: false
+    t.integer "diet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_recipes_on_category_id"
+    t.index ["diet_id"], name: "index_recipes_on_diet_id"
+  end
+
+  add_foreign_key "recipes", "categories"
+  add_foreign_key "recipes", "diets"
 end
